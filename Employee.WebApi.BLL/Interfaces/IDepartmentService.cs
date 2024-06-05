ebdc0491edcd5247840de880a56b2aa1dc1b0a1f
Lekyasri <lekyasri.c@technovert.com>
2024-06-05 18:47:52 +0530

    Added webapis

--- Employee.WebApi.BLL/Interfaces/IDepartmentService.cs
+++
@@ -1,9 +0,0 @@
-﻿using EmployeeConsole.BLL.DataTransferObjects;
-
-namespace EmployeeConsole.BLL.Interfaces
-{
-    public interface IDepartmentService
-    {
-        public bool AddDepartment(DepartmentDTO Department);
-    }
-}
--- Employee.WebApi.BLL/Interfaces/ILocationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using EmployeeConsole.BLL.DataTransferObjects;
-
-namespace EmployeeConsole.BLL.Interfaces
-{
-    public interface ILocationService
-    {
-        public bool AddLocation(LocationDTO LocationName);
-    }
-}
